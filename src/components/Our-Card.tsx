8f8973db558ebbc4fbc1499e8e4d288311572166
Ayush-Khedekar <ayushkhedekar07@gmail.com>
2025-08-14 11:27:25 +0530

    Money Care change

--- src/components/Our-Card.tsx
+++ src/components/Our-Card.tsx
@@ -1,41 +1,15 @@
-// import React from "react";
-// import NewCard from "./NewCard";
+
 import ServiceCarousel from "./ServiceCarousel";
-// const dataType = [
-//   {
-//     Icons: "",
-//     Title: "Our Mission",
-//     Content:
-//       "Our mission is to provide a seamless, unified platform for bill payments, mobile recharges, and a wide range of utility services. We aim to simplify the technology for individuals and businesses by offering a one-stop solution that is fast, secure, and accessible.",
-//   },
-//   {
-//     Icons: "",
-//     Title: "Our Values",
-//     Content:
-//       "We believe in building trust with our users by providing transparent and secure financial services. We are committed to delivering top-quality services while upholding the highest standards of integrity and customer satisfaction.",
-//   },
-//   {
-//     Icons: "",
-//     Title: "Our Vision",
-//     Content:
-//       "Empowering seamless lifestyles through smart utility solutions and transformative travel experiences. We envision a world where essential services and exploration merge effortlessly, creating convenience, affordability, and unmatched accessibility for every journey and daily need.",
-//   },
-//   {
-//     Icons: "",
-//     Title: "Our Achievements",
-//     Content:
-//       "Since launching, we’ve helped countless users seamlessly pay bills, recharge mobile phones, and more—making their travel faster and simpler. Our platform has become the go-to solution for businesses and individuals, offering integrated, secure, and easy-to-use services.",
-//   },
-// ];
+
 
 const OurCard = () => {
   return (
     <>
       <div className="text-center bg-blue-50">
-        <p className="text-lg tracking-widest text-gray-400 uppercase">
+        <p className="text-[1rem] tracking-widest text-[#133042] uppercase">
           [ READY TO GET STARTED ]
         </p>
-        <h2 className="text-3xl sm:text-4xl text-blue-800 leading-snug font-bold">
+        <h2 className="text-3xl sm:text-4xl text-[#133042] leading-snug font-bold">
           Empowering change, one step at <br /> a time.
         </h2>
       </div>
@@ -59,10 +33,10 @@ const OurCard = () => {
                 settled in seconds.
               </p>
 
-              <button className="bg-[#7a5af8] hover:bg-[#6747e9] text-white px-6 py-3 rounded-full font-semibold text-sm flex items-center gap-2 transition duration-300">
+              <a href="#CONTACT"><button className="bg-[#7a5af8] hover:bg-[#6747e9] text-white px-6 py-3 rounded-full font-semibold text-sm flex items-center gap-2 transition duration-300">
                 Get in touch
                 <span className="text-lg">→</span>
-              </button>
+              </button></a>
             </div>
           </div>
         </div>
