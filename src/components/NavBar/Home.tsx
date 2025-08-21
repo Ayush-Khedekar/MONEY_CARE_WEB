ba1884d2f91ee89e0a0147c08f5a93756ccde754
Ayush <ayushkhedekar07@gmail.com>
2025-08-21 16:20:32 +0530

    Final_Money_Care

--- src/components/NavBar/Home.tsx
+++ src/components/NavBar/Home.tsx
@@ -2,9 +2,9 @@ import { images } from "../../image-export/Images";
 
 const Home = () => {
   return (
-    <div className="bg-[#f0f6ff] overflow-hidden md:p-5 flex flex-col md:flex-row items-center justify-between w-full gap-4">
-      <div className="w-full sm:w-[70%] md:w-[40%] space-y-5 p-4 sm:p-6 font-sans transition-all duration-500 ease-in-out">
-        <h1 className="text-2xl xs:text-3xl sm:text-4xl md:text-5xl font-bold leading-snug tracking-tight">
+    <div className="bg-[#f0f6ff] overflow-hidden md:p-5 flex flex-col md:flex-row md:items-center justify-between w-full gap-4">
+      <div className="w-full sm:w-full md:w-[50%] space-y-5 p-4 sm:p-6 font-sans transition-all duration-500 ease-in-out">
+        <h1 className="text-2xl xs:text-3xl sm:text-4xl md:text-4xl font-bold leading-snug tracking-tight">
           <span className="text-[#133042] block w-full font-bold">
             Streamline Your Life:
           </span>
@@ -17,7 +17,7 @@ const Home = () => {
           </span>
         </h1>
 
-        <p className="text-gray-700 mt-4 sm:mt-6 text-base sm:text-lg leading-relaxed">
+        <p className="text-gray-700 mt-4 sm:mt-6 text-base sm:text-[0.9rem] md:text-[1.1rem] leading-relaxed">
           Planning with <strong className="text-[#15683A]">Money Care</strong> &
           our advanced technology and secure systems provide a safe and reliable
           way to transfer funds, allowing you to benefit from reduced processing
@@ -31,52 +31,47 @@ const Home = () => {
         </a>
       </div>
 
-      <div className="bg-[#f0f6ff] p-1 flex items-center justify-center w-full  md:w-[60%]">
+      <div className="bg-[#f0f6ff] p-1 flex items-center justify-center w-full md:w-[50%]">
         <div className="relative w-full">
-          <div className="flex flex-row justify-center items-start gap-2 sm:gap-4  md:gap-10 mb-8">
+          <div className="flex flex-row justify-center items-start gap-2 sm:gap-4 md:gap-5 mb-8">
             <div className="bg-white p-4 rounded-2xl shadow-md flex flex-col items-center transition-shadow duration-300 hover:shadow-xl shadow-gray-400 w-full sm:w-auto">
               <img
                 src={images.Credit}
                 alt="Mike"
-                className="w-20 h-20 sm:w-24 sm:h-24 rounded-xl object-contain"
+                className="w-25 sm:w-30 rounded-xl object-contain"
               />
-              <div className="mt-2 text-center  ">
-                <p className="text-sm font-semibold ">Mike</p>
-                <p className="text-xs text-gray-500 font-bold">Game Ninja</p>
+              <div className="text-center">
+                <p className="text-sm font-semibold ">Utility Payments</p>
               </div>
             </div>
 
-            <div className="bg-white p-4 sm:p-6 rounded-2xl shadow-md w-full sm:w-64 text-center transition-shadow duration-300 hover:shadow-xl shadow-gray-400">
+            <div className="bg-white p-4 sm:p-6 rounded-2xl shadow-md w-full sm:w-56 text-center transition-shadow duration-300 hover:shadow-xl shadow-gray-400">
               <img
                 src={images.Money}
                 alt="Controller"
-                className="w-20 h-20 sm:w-24 sm:h-24 object-contain mx-auto mb-2"
+                className="w-30 sm:w-30 object-contain mx-auto mb-2"
               />
               <div className="flex flex-col gap-2">
-                <p className="text-gray-600 text-xs sm:text-sm font-medium">
-                  PS 4 Controller
-                </p>
-                <p className="text-md sm:text-xl font-bold ">₹ 75.00</p>
-                <button className="bg-gray-900 text-white px-2 sm:px-4 py-2 rounded-2xl text-xs sm:text-sm font-medium hover:bg-gray-800 transition-colors">
-                  PAY BY BANK
+                <button className="bg-gray-900 text-white px-2 sm:px-4 py-2 rounded-2xl text-xs sm:text-sm font-medium hover:bg-gray-800 transition-colors cursor-pointer">
+                  Recharge
                 </button>
               </div>
             </div>
 
-            <div className="bg-white p-4 rounded-2xl shadow-md w-full sm:w-40 text-center font-semibold transition-shadow duration-300 hover:shadow-xl shadow-gray-400">
-              <p className="text-xs sm:text-sm font-semibold mb-3 ">
-                Select your bank
+            <div className="bg-white p-4 rounded-2xl shadow-md w-full sm:w-auto text-center font-semibold transition-shadow duration-300 hover:shadow-xl shadow-gray-400">
+              <p className="text-xs sm:text-sm font-semibold mb-3">
+                Tours and Travels
               </p>
               <img
                 src={images.MoneyStress}
                 alt="Bank Logos"
-                className="w-full h-auto object-contain"
+                className="w-25 sm:w-30 object-contain"
               />
             </div>
           </div>
 
           <div className="flex justify-between items-center gap-4 sm:gap-5 mt-8">
-            <div className="bg-white p-4 sm:p-6 rounded-2xl shadow-md w-full sm:w-96 text-xs sm:text-sm transition-shadow duration-300 hover:shadow-xl shadow-gray-400">
+            <div className="bg-white p-4 sm:p-6 rounded-2xl shadow-md w-full sm:w-[50%] md:w-96 text-xs sm:text-sm transition-shadow duration-300 hover:shadow-xl shadow-gray-400">
               <p className="text-gray-700 mb-2 text-[1rem] font-bold">
                 Authorize payment
               </p>
